878ccf0e7d1dae5269cad1e18a906fec50164c59
JackStandley <jspr5@umsystem.edu>
2024-11-08 12:46:08 -0600

    Loads generated svg chart in the same page

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # app.py
-from flask import Flask, render_template, request, jsonify, redirect, flash
+
+from flask import Flask, render_template, request, jsonify, flash, redirect
 from main import run_chart_generation  # Import the modified function from main.py
 import csv
 
@@ -9,7 +10,7 @@ app.config['SECRET_KEY'] = 'your secret key'
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', chart_svg=None)  # Include an optional chart_svg parameter
 
 @app.route('/api/symbols')
 def get_symbols():
@@ -23,20 +24,23 @@ def get_symbols():
 @app.route('/generate_chart', methods=['POST'])
 def generate_chart():
     try:
-        # Extract form data from the request
         symbol = request.form.get('symbol')
         chart_type = request.form.get('chartType')
         time_series = request.form.get('timeSeries')
         start_date = request.form.get('startDate')
         end_date = request.form.get('endDate')
 
-        
+        # Generate chart SVG
+        svg_chart = run_chart_generation(symbol, chart_type, time_series, start_date, end_date)
 
-        run_chart_generation(symbol, chart_type, time_series, start_date, end_date)
-
-        return redirect('/') 
+        if svg_chart:
+            return render_template('index.html', chart_svg=svg_chart)
+        else:
+            flash("Error generating chart data.")
+            return redirect('/')
     except Exception as e:
-        flash(f'Error generating chart: Please check start and end dates')
+        flash(f'Error generating chart: {e}')
+        return redirect('/')
 
 if __name__ == '__main__':
     app.run(port=5008)
--- main.py
+++ main.py
@@ -1,20 +1,18 @@
+# main.py
 
 from app_data import set_stock_symbol, set_chart_type, set_time_series, set_dates, get_chart_type
 from api_handler import fetch_stock_data
 from chart_generator import generate_chart
 
 def run_chart_generation(symbol, chart_type, time_series, start, end):
-    # Set values based on passed parameters
     set_stock_symbol(symbol)
     set_chart_type(chart_type)
     set_time_series(time_series)
     set_dates(start, end)
 
-    # Fetch and generate chart
     fetch_stock_data()
     try:
-        generate_chart(get_chart_type())
-        print("Chart generated successfully.")
+        return generate_chart(get_chart_type())
     except ValueError as e:
         print(f"Error generating chart: {e}")
-
+        return None
